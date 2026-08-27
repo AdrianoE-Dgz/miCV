d57f503bc1f824d349927bc05cad34288bb65a0d
AdrianoE-Dgz <areliedgz5005@gmail.com>
2026-08-26 20:37:07 -0600

    Feat: Descripción casi terminada, PDF incluido

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,66 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
+import { Footer } from './components/footer/footer';
+import { Task } from "./components/task/task";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [Navbar, Footer, Task],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('eli-curriculum');
+
+  protected experiencia1 = [
+    {
+      title: "Titulo",
+      date: "",
+      desc: `
+        <p>Esto es parte de la descripción</p>
+        <p>Puede ser más o menos texto</p>
+      `
+    },
+    {
+      title: "Titulo",
+      date: "",
+      desc: `
+        <p>Esto es parte de la descripción</p>
+        <p>Puede ser más o menos texto</p>
+      `
+    },
+    {
+      title: "Titulo",
+      date: "",
+      desc: `
+        <p>Esto es parte de la descripción</p>
+        <p>Puede ser más o menos texto</p>
+      `
+    },
+    {
+      title: "Titulo",
+      date: "",
+      desc: `
+        <p>Esto es parte de la descripción</p>
+        <p>Puede ser más o menos texto</p>
+      `
+    },
+    {
+      title: "Titulo",
+      date: "",
+      desc: `
+        <p>Esto es parte de la descripción</p>
+        <p>Puede ser más o menos texto</p>
+      `
+    },
+    {
+      title: "Titulo",
+      date: "",
+      desc: `
+        <p>Esto es parte de la descripción</p>
+        <p>Puede ser más o menos texto</p>
+      `
+    },
+  ]
+
 }
